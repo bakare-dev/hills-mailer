c8780c84269bc71b60a36683969df95a76700e94
112479953+bakare-dev@users.noreply.github.com <112479953+bakare-dev@users.noreply.github.com>
2024-04-15 20:08:01 +0100

    mailer update

--- services/NotificationService.js
+++ services/NotificationService.js
@@ -22,7 +22,7 @@ class NotificationService {
         message.recipients.forEach(async (item) => {
 
             let info = {
-                sender: "noreply@classoptima.bakare.tech",
+                sender: "noreply@hill.dev",
                 templateFile: "form-submit.ejs",
                 subject: "Form Submission",
                 recipients: item,
